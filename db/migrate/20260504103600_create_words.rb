30cbd65a64e470ba3e804b4dc3a9f41bc5bc1e6f
celtic93 <egorka80@list.ru>
2026-06-05 17:47:29 +0300

    Added parsed_definitions to Word

--- db/migrate/20260504103600_create_words.rb
+++ db/migrate/20260504103600_create_words.rb
@@ -2,6 +2,7 @@ class CreateWords < ActiveRecord::Migration[8.1]
   def change
     create_table :words, comment: "Keeps English words" do |t|
       t.text :text, null: false, comment: "The Word itself"
+      t.jsonb :parsed_definitions, comment: "The definitions of Word parsed from dictionary"
 
       t.timestamps
     end
